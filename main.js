da4686737e734c67654189d1ce054b5f5ecabf25
Ngannguyendt2 <ngannguyendt2haui@gmail.com>
2019-08-20 08:08:35 +0700

    up file on lamp

---
+++ main.js
@@ -0,0 +1,32 @@
+let ElectricLamp = function (statusLamp) {
+    this.statusLamp = statusLamp;
+    this.turnOff = function () {
+        this.statusLamp='dark';
+        document.getElementById('lamp').innerHTML=this.statusLamp;
+    }
+
+    this.turnOn = function () {
+
+         this.statusLamp = 'light';
+         document.getElementById('lamp').innerHTML=this.statusLamp;
+    };
+};
+let SwitchButton = function (statusSwitch, lamp) {
+    this.statusSwitch = statusSwitch;
+    this.lamp = lamp;
+    this.connectToLamp = function (lamp) {
+        this.lamp = lamp;
+    };
+    this.switchOff = function () {
+        this.statusSwitch='false';
+         this.lamp.turnOff();
+    };
+    this.switchOn = function () {
+        this.statusSwitch='true';
+         this.lamp.turnOn();
+    };
+};
+
+let lamp = new ElectricLamp('false');
+let btnswitch = new SwitchButton(false,lamp);
+
