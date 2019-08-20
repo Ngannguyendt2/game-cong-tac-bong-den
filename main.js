c3ef9801bc67e894c1a0063c7e42b9d3e92826cc
Ngannguyendt2 <ngannguyendt2haui@gmail.com>
2019-08-20 08:41:21 +0700

    on-off lamp

--- main.js
+++ main.js
@@ -1,32 +1,36 @@
 let ElectricLamp = function (statusLamp) {
     this.statusLamp = statusLamp;
     this.turnOff = function () {
-        this.statusLamp='dark';
-        document.getElementById('lamp').innerHTML=this.statusLamp;
+        this.statusLamp = 'dark';
+        document.getElementById('lamp').innerHTML = this.statusLamp;
     }
 
     this.turnOn = function () {
 
-         this.statusLamp = 'light';
-         document.getElementById('lamp').innerHTML=this.statusLamp;
+        this.statusLamp = 'light';
+        document.getElementById('lamp').innerHTML = this.statusLamp;
     };
 };
 let SwitchButton = function (statusSwitch, lamp) {
     this.statusSwitch = statusSwitch;
     this.lamp = lamp;
-    this.connectToLamp = function (lamp) {
-        this.lamp = lamp;
-    };
     this.switchOff = function () {
-        this.statusSwitch='false';
-         this.lamp.turnOff();
+        this.statusSwitch = false;
+        this.lamp.turnOff();
     };
     this.switchOn = function () {
-        this.statusSwitch='true';
-         this.lamp.turnOn();
+        this.statusSwitch = true;
+        this.lamp.turnOn();
     };
+    this.switch = function () {
+        if (this.statusSwitch) {
+            this.switchOff();
+        } else {
+            this.switchOn();
+        }
+    }
 };
 
 let lamp = new ElectricLamp('false');
-let btnswitch = new SwitchButton(false,lamp);
+let btnswitch = new SwitchButton(false, lamp);
 
